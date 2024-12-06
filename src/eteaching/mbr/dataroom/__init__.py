89ba2c3aa9bec2e9b4faa81e2d91945df845df95
Markus Schmidt <m.schmidt@iwm-tuebingen.de>
2024-12-06 12:43:10 +0100

    Added: Data room connection

--- src/eteaching/mbr/dataroom/__init__.py
+++ src/eteaching/mbr/dataroom/__init__.py
@@ -4,3 +4,5 @@ from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('eteaching.mbr.dataroom')
+
+PACKAGE_NAME = "eteaching.mbr.dataroom"
---
+++ src/eteaching/mbr/dataroom/logger.py
@@ -0,0 +1,4 @@
+from eteaching.mbr.dataroom import PACKAGE_NAME
+import logging
+
+LOG = logging.getLogger(PACKAGE_NAME)
